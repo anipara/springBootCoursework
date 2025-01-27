e4bc1c23e15e40b1bf6364a2b0b33125fafae8d8
aparasrampuria <aparasrampuria@caesars.com>
2025-01-27 11:43:58 -0800

    MVC model data binding

--- thymeleafdemo/thymeleafdemo/src/main/java/com/springcourse/thymeleafdemo/controller/HelloWorldController.java
+++ thymeleafdemo/thymeleafdemo/src/main/java/com/springcourse/thymeleafdemo/controller/HelloWorldController.java
@@ -1,8 +1,11 @@
 package com.springcourse.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -16,5 +19,32 @@ public class HelloWorldController {
     @RequestMapping("/processForm")
     public String processForm() {
         return "helloworld";
+    }
+
+    /*
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShout(HttpServletRequest request, Model model){
+
+        String name = request.getParameter("studentName");
+
+        String result = "YO " +  name.toUpperCase();
+
+        model.addAttribute("message", result);
+
+        return "helloworld";
+
+    }
+    */
+
+    @RequestMapping("/processFormVersionThree")
+    // Directly bind the param coming from the form to the parameter
+    public String letsShoutWithBinding(@RequestParam("studentName") String name, Model model){
+
+        String result = "Were binding this time " +  name.toUpperCase();
+
+        model.addAttribute("message", result);
+
+        return "helloworld";
+
     }
 }

39a310cdd9166377a48080eb30fb075b069fdffd
aparasrampuria <aparasrampuria@caesars.com>
2025-01-30 09:27:32 -0800

    adding student model and views

--- thymeleafdemo/thymeleafdemo/src/main/java/com/springcourse/thymeleafdemo/controller/HelloWorldController.java
+++ thymeleafdemo/thymeleafdemo/src/main/java/com/springcourse/thymeleafdemo/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,7 +17,7 @@ public class HelloWorldController {
     }
 
     // need a controller method to process the HTML form
-    @RequestMapping("/processForm")
+    @PostMapping("/processForm")
     public String processForm() {
         return "helloworld";
     }
---
+++ thymeleafdemo/thymeleafdemo/src/main/java/com/springcourse/thymeleafdemo/controller/StudentController.java
@@ -0,0 +1,24 @@
+package com.springcourse.thymeleafdemo.controller;
+
+import com.springcourse.thymeleafdemo.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StudentController {
+
+    @GetMapping("/showStudentForm")
+    public String showForm(Model model){
+
+        model.addAttribute("student", new Student());
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student student ){
+        return "student-confirmation";
+    }
+}

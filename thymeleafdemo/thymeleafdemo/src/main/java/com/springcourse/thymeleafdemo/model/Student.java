9ac48a56198fdb3bcfd3286664bb86dc27189f42
aparasrampuria <aparasrampuria@caesars.com>
2025-01-30 14:11:42 -0800

    adding form input and rendering in view

--- thymeleafdemo/thymeleafdemo/src/main/java/com/springcourse/thymeleafdemo/model/Student.java
+++ thymeleafdemo/thymeleafdemo/src/main/java/com/springcourse/thymeleafdemo/model/Student.java
@@ -1,9 +1,14 @@
 package com.springcourse.thymeleafdemo.model;
 
+import java.util.List;
+
 public class Student {
 
     private String firstName;
     private String lastName;
+    private String country;
+    private String favoriteLanguage;
+    private List<String> favoriteSystems;
 
     public Student() {
     }
@@ -22,5 +27,29 @@ public class Student {
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
+    }
+
+    public List<String> getFavoriteSystems() {
+        return favoriteSystems;
+    }
+
+    public void setFavoriteSystems(List<String> favoriteSystems) {
+        this.favoriteSystems = favoriteSystems;
     }
 }
